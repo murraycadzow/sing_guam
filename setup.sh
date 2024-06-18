f3d380e29e69b7d92997a3190c5c8e709784c1ad
Murray Cadzow <murray.cadzow@otago.ac.nz>
2024-06-18 16:31:01 +1200

    start of setup script - not tested

--- setup.sh
+++ setup.sh
@@ -1,5 +1,21 @@
 # $1 is the account list file
 
+BASE=/projects/teaching/sing_guam
+
+## Make conda available
+mkdir -p ${BASE}/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ${BASE}/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ${BASE}/miniconda3
+rm -rf ${BASE}/miniconda3/miniconda.sh
+
+## Create conda environment
+source ${BASE}/miniconda3/etc/profile.d/conda.sh
+conda create -p ${BASE}/workshop_env
+conda activate ${BASE}/workshop_env
+conda install --libsolver=mamba -c conda-forge -c bioconda samtools bcftools vcftools tabix
+
+
+## Create a working area for each account
 for account in $(cat $1)
 do
 	echo ${account}
