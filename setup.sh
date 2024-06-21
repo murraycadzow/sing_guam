a09847ea66a0cacaed49d54e7e75f20df0a7d5d2
Murray Cadzow <murray.cadzow@otago.ac.nz>
2024-06-21 14:21:40 +1200

    create conda environment

--- setup.sh
+++ setup.sh
@@ -5,14 +5,14 @@ BASE=/projects/teaching/sing_guam
 ## Make conda available
 mkdir -p ${BASE}/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ${BASE}/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ${BASE}/miniconda3
+bash ${BASE}/miniconda3/miniconda.sh -b -u -p ${BASE}/miniconda3
 rm -rf ${BASE}/miniconda3/miniconda.sh
 
 ## Create conda environment
 source ${BASE}/miniconda3/etc/profile.d/conda.sh
 conda create -p ${BASE}/workshop_env
 conda activate ${BASE}/workshop_env
-conda install --libsolver=mamba -c conda-forge -c bioconda samtools bcftools vcftools tabix
+conda install --solver=libmamba -c conda-forge -c bioconda samtools bcftools vcftools tabix eigensoft plink
 
 
 ## Create a working area for each account
