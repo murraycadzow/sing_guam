095f38d2be3b309adda53de56b0aff83a34ced0a
Murray Cadzow <murray.cadzow@otago.ac.nz>
2024-07-15 17:08:15 +1200

    update to use miniforge env

--- load_software.sh
+++ load_software.sh
@@ -2,6 +2,6 @@
 
 BASE=/projects/teaching/sing_guam                                   
 ## Make conda available
-source ${BASE}/miniconda3/etc/profile.d/conda.sh
-conda activate ${BASE}/workshop_env
+source ${BASE}/miniforge3/etc/profile.d/conda.sh
+conda activate ${BASE}/sing_env
 echo -e "Software for SING Guam now loaded."
